ef94a0bce1d164eb15963729f83d2e81c851b956
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-11-27 17:57:51 -0500

    Refactor into pages with react router

--- src/App.js
+++ src/App.js
@@ -1,31 +1,18 @@
 import './App.css';
-import idPic from './assets/id-pic.jpeg'
-import map from './assets/map.jpeg'
+import Termites from './pages/termites';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
 
 function App() {
   return (
-    <div className="App bg-cyan-700 h-screen pt-12 px-8">
-      <header className="App-header text-center mb-14 flex-wrap">
-        <h1 className='font-light text-4xl tracking-wider w-full text-gray-100'>Termite or Pest Auto-Identifier?</h1>
-        <p className='text-white'>Find Out Whether That Pest You Found is an Ant or a Termite Eating Your Home</p>
-      </header>
-      <section>
-        <div className='grid grid-cols-5 gap-10 px-5'>
-          <div className='bg-white rounded-lg shadow-lg px-12 py-10 col-span-3'>
-            <h2 className='text-2xl font-light text-gray-700 mb-4'>Why Termites Matter</h2>
-            <p className='text-gray-700 mb-8'>With termites causing an estimated $5 billion in damage to homes each year in the United States, it's clear that these pests are a major problem. And yet, many homeowners are unaware of the importance of termite control until it's too late.</p>
-            <p className='text-gray-700'>That's why it's important to be proactive about termite control and to make sure that your home is covered by a comprehensive termite treatment plan. By catching termites early and preventing them from doing further damage, you can save yourself a lot of money – and a lot of headaches.</p>
-          </div>
-          <div className="col-span-2">
-            <img src={map} alt="A map of termite activity in the USA" />
-          </div>
-        </div>
-      </section>
-      <section className='flex justify-center flex-wrap py-12'>
-        <h2 className='font-light text-xl tracking-wider w-full text-gray-100 text-center'>How to Tell the Difference?</h2>
-        <p className='text-white w-full text-center  mb-4'>How to visually ID the difference between termites and ants (or take a picture below!)</p>
-        <img src={idPic} alt="A picture of a termite and an ant" className='w-1/2' />
-      </section>
+    <div className="App bg-cyan-700 h-screen pt-12 px-16">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/termites" element={<Termites />} />
+          <Route path='*' element={<h1>404: Not Found</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/termites.js
@@ -0,0 +1,10 @@
+function Termites() {
+    return (
+        <div>
+            <h1>Uh Oh! You Have Termites!</h1>
+            <p>Termites are a serious problem. They can cause thousands of dollars in damage to your home. If you have termites, you need to take action immediately.</p>
+        </div>
+    )
+}
+
+export default Termites;
