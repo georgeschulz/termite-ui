3d106003812e803703bbb4659d1640b307e7c725
97812180+georgeschulz@users.noreply.github.com <97812180+georgeschulz@users.noreply.github.com>
2022-11-28 19:35:24 -0500

    Fix p spacing on termites page

--- src/pages/termites.js
+++ src/pages/termites.js
@@ -8,10 +8,10 @@ function Termites() {
             link="https://www.bettertermite.com/northern-virginia-termite-control/"
             subheader="Find Out Whether That Pest You Found is an Ant or a Termite Eating Your Home"
         >
-            <p className="w-full">Every moment termites are in your home, they are doing damage to your home's structure and causing damange.</p>
-            <p className="w-full">You should take immediate action and book an estimate to have the termites removed. This is a <b>free</b> and easy process, usually taking less than an hour!</p>
-            <p className="w-full">However, waiting to take action can mean the actual repair costs are higher than if you waited.</p>
-            <p className="w-full">New customers get $200 off their treatment and a free estimate. Click below to book an estimate and claim the coupoun!</p>
+            <p className="w-full mb-4">Every moment termites are in your home, they are doing damage to your home's structure and causing damange.</p>
+            <p className="w-full mb-4">You should take immediate action and book an estimate to have the termites removed. This is a <b>free</b> and easy process, usually taking less than an hour!</p>
+            <p className="w-full mb-4">However, waiting to take action can mean the actual repair costs are higher than if you waited.</p>
+            <p className="w-full mb-4">New customers get $200 off their treatment and a free estimate. Click below to book an estimate and claim the coupoun!</p>
         </TargetLayout>
     )
 }
